f76478b88e6cb923aadeed7ce84e3571635377e8
Darrkkk09 <ranjitmutchakarla123@gmail.com>
2025-05-11 22:36:15 +0530

    app.js

--- app.js
+++ app.js
@@ -75,7 +75,7 @@ io.on('connection', (uniqueSocket) => {
     });
 });
 const PORT = process.env.PORT || 3000;
-
-server.listen(port, () => {
-    console.log("Server is running on port 3000");
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
+
